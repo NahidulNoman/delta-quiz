a3fdd940d01aadefc69f97827956420de1e0a496
NahidulNoman <nahidulna5@gmail.com>
2022-10-11 09:21:12 +0600

    icon correct show

--- src/Components/Quizzes/Quizzes.jsx
+++ src/Components/Quizzes/Quizzes.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 
+
 const Quizzes = ({perQus}) => {
-    console.log(perQus)
     const {question,correctAnswer,options} = perQus;
+    
+    const correctAns = (correct) => {
+        alert(correct)
+    }
+
     return (
         <div className='container'>
             <div className='shadow-lg m-5 p-3 rounded-3 bg-white'>
                 <p className='text-center fw-semibold '>
                    {question}
-                   <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+                   <FontAwesomeIcon onClick={()=>correctAns(correctAnswer)} icon={faEye}></FontAwesomeIcon>
                 </p>
                 <div>
                     {
-                        options.map(option => <div class="form-check">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-                        <label class="form-check-label" for="flexRadioDefault1">
+                        options.map(option => <div className="form-check">
+                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
                           {option}
                         </label>
                       </div>)
