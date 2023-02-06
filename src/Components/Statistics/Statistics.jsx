7d6616cb3dfe888231177b69f35bbc3a8d15c682
NahidulNoman <nahidulna5@gmail.com>
2023-02-06 23:24:09 +0600

    statistic modify

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
-    const statistic = useLoaderData();
-    const statistics = statistic.data;
-    return (
-        <div className='container mt-5'>
-            <ResponsiveContainer width={'99%'} height={400}>
-            <AreaChart 
-          data={statistics}>
-         <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
+  const statistic = useLoaderData();
+  const statistics = statistic.data;
+  return (
+    <div className="container mt-5">
+      <ResponsiveContainer width={"99%"} height={400}>
+        <AreaChart data={statistics}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
-            </AreaChart>
-            </ResponsiveContainer>
-        </div>
-    );
+          <Area
+            type="monotone"
+            dataKey="total"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Statistics;
