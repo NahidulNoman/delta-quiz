88c0d3a75e1ff25f2eb934d78a5822bb41cce1dc
NahidulNoman <nahidulna5@gmail.com>
2023-02-07 11:06:13 +0600

    navbar modify

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,35 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        
-            <nav className="navbar navbar-expand-lg bg-light p-3 shadow-lg">
-                <div className="container">
-                    <Link className="navbar-brand fw-bold" to="/">DeltaQuiz</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                        <Link className="nav-link fw-semibold" to="/home" title='Home'>Home</Link>
-                        </li>
-                        <li className="nav-item">
-                        <Link className="nav-link fw-semibold" to="/topics" title='Topics'>Topics</Link>
-                        </li>
-                        <li className="nav-item">
-                        <Link className="nav-link fw-semibold" to="/statistics" title='Statistics'>Statistics</Link>
-                        </li>
-                        <li className="nav-item">
-                        <Link className="nav-link fw-semibold" to="/blog" title='Blog'>Blog</Link>
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-                </nav>
-       
-    );
+  return (
+    <nav className="navbar navbar-expand-lg bg-light p-3 shadow-lg">
+      <div className="container">
+        <Link className="navbar-brand fw-bold" to="/">
+          DeltaQuiz
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse d-lg-flex justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link fw-semibold" to="/home" title="Home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link fw-semibold"
+                to="/topics"
+                title="Topics"
+              >
+                Topics
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link fw-semibold"
+                to="/statistics"
+                title="Statistics"
+              >
+                Statistics
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link fw-semibold" to="/blog" title="Blog">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
