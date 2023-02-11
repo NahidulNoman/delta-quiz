063ecef53c63baedeb541f44e2c40925f54e8b2c
NahidulNoman <nahidulna5@gmail.com>
2023-02-11 22:19:59 +0600

    quiz details

--- src/Components/QuizDetails/QuizDetails.jsx
+++ src/Components/QuizDetails/QuizDetails.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Questions from '../Questions/Questions';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Questions from "../Questions/Questions";
 
 const QuizDetails = () => {
-    const getQuizzes = useLoaderData();
-    const ques = getQuizzes.data;
-    return (
-        <div>
-            <Questions ques={ques}></Questions>
-        </div>
-    );
+  const getQuizzes = useLoaderData();
+  const ques = getQuizzes.data;
+  return (
+    <div>
+      <Questions ques={ques}></Questions>
+    </div>
+  );
 };
 
 export default QuizDetails;
